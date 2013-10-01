c623c7702eaffb841e220dceb7f3f5ce48823a6a
Erick Santillan <rederick2@hotmail.com>
2013-10-01 11:06:32 -0500

    cambio titulo

--- protected/config/main.php
+++ protected/config/main.php
@@ -9,7 +9,7 @@ Yii::setPathOfAlias('bootstrap', dirname(__FILE__).'/../extensions/bootstrap');
 
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Linea de Tiempo',
+	'name'=>'Mi Linea de Tiempo',
 
 	// preloading 'log' component
 	'preload'=>array('log'),

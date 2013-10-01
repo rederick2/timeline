2b38179fe07f981151a075ca94f0320f8d83c478
Erick Santillan <rederick2@hotmail.com>
2013-10-01 10:36:07 -0500

    cambio index

--- themes/bootstrap/views/site/index.php
+++ themes/bootstrap/views/site/index.php
@@ -12,8 +12,6 @@ $this->pageTitle=Yii::app()->name;
 
 <?php $this->endWidget(); ?>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-
 <ul>
     <li>View file: <code><?php echo __FILE__; ?></code></li>
     <li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
